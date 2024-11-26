cfbab9cb50763635e1fd6932ceec22abe4a0088f
刘力瑞 <admin@liulirui.com>
2024-11-26 11:32:54 +0800

    Update App.tsx

--- ReproducerApp/App.tsx
+++ ReproducerApp/App.tsx
@@ -15,6 +15,7 @@ import {
   Text,
   useColorScheme,
   View,
+  Image,
 } from 'react-native';
 
 import {
@@ -68,30 +69,17 @@ function App(): React.JSX.Element {
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+      <Image
+        style={{ width: 100, height: 100 }}
+        source={{
+          uri: `https://reactnative.dev/img/tiny_logo.png?r=${Math.random()}`,
+        }}
+        onLoad={() => console.warn('onLoad')}
+        onLoadStart={() => console.warn('onLoadStart')}
+        onLoadEnd={() => console.warn('onLoadEnd')}
+        onProgress={() => console.warn('onProgress')}
+        onError={() => console.warn('onError')}
+      />
     </SafeAreaView>
   );
 }
